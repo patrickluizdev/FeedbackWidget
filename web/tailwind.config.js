19a8b4dc6d806c1d7fabe761809ba5c64f65cd51
patrickluizdev <patrick@fasters.com.br>
2024-03-13 18:49:37 -0300

    feat: :wrench: add config theme tailwind

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize:{
+        'xs': 12,
+        'sm': 14,
+        'md': 16,
+        'lg': 20,
+      },
+      colors:{
+        'black': '#000',
+        'white': '#FFF',
+        'transparent': 'transparent',
+        brand: {
+          500: '#8257E5',
+          300: '#996DFF',
+          "on-brand-color": '#FFF'
+        },
+      },
+      fontFamily:{
+        sans: 'Inter, sans-serif'
+      }
+    },
   },
   plugins: [],
 }
