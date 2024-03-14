559c603503d4045f0f812712895b03b4b4c793d5
patrickluizdev <patrick@fasters.com.br>
2024-03-13 22:34:46 -0300

    style: :zap: template theme

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -14,10 +14,21 @@ export default {
         'white': '#FFF',
         'transparent': 'transparent',
         brand: {
-          500: '#8257E5',
-          300: '#996DFF',
+          "primary": '#8257E5',
+          "hover": '#996DFF',
           "on-brand-color": '#FFF'
         },
+        dark: {
+          'surface-primary' : '#18181b',
+          'surface-secondary': '#27272a',
+          'surface-secondary-hover': '#3f3f46',
+          'surface-dark' : '#121212',
+          'stroke': '#52525b',
+          'tooltip': '#f4f4f5',
+          'text-primary' : '#f4f4f5',
+          'text-secondary': '#a1a1aa',
+          'text-on-tooltip' : '#27272a',
+        }
       },
       fontFamily:{
         sans: 'Inter, sans-serif'
